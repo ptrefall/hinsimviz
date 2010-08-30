632140342a301f77e0a0343ce1399406c740b78e
PTrefall@gmail.com <PTrefall@gmail.com@28a56cb3-1e7c-bc60-7718-65c159e1d2df>
2010-08-30 11:21:19 +0000

    First commit. This commit has only displays a Qt window with a GLWidget integrated. Implementation is based on Qt 4.6.3

---
+++ trunk/Src/QtTemp/moc/moc_UiMainWindow.h
@@ -0,0 +1,112 @@
+/********************************************************************************
+** Form generated from reading UI file 'MainWindow.ui'
+**
+** Created: Mon 30. Aug 13:12:26 2010
+**      by: Qt User Interface Compiler version 4.6.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef MOC_UIMAINWINDOW_H
+#define MOC_UIMAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QGridLayout>
+#include <QtGui/QHeaderView>
+#include <QtGui/QLabel>
+#include <QtGui/QMainWindow>
+#include <QtGui/QMenu>
+#include <QtGui/QMenuBar>
+#include <QtGui/QSpacerItem>
+#include <QtGui/QStatusBar>
+#include <QtGui/QWidget>
+#include "QtExtensions/GLWidget.h"
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *action_close;
+    QWidget *centralwidget;
+    QGridLayout *gridLayout;
+    GLWidget *scene;
+    QGridLayout *gridLayout_2;
+    QSpacerItem *verticalSpacer;
+    QSpacerItem *horizontalSpacer;
+    QLabel *force_label;
+    QMenuBar *menubar;
+    QMenu *menu_File;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(1215, 640);
+        action_close = new QAction(MainWindow);
+        action_close->setObjectName(QString::fromUtf8("action_close"));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        gridLayout = new QGridLayout(centralwidget);
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        scene = new GLWidget(centralwidget);
+        scene->setObjectName(QString::fromUtf8("scene"));
+        gridLayout_2 = new QGridLayout(scene);
+        gridLayout_2->setObjectName(QString::fromUtf8("gridLayout_2"));
+        verticalSpacer = new QSpacerItem(20, 410, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        gridLayout_2->addItem(verticalSpacer, 1, 2, 1, 1);
+
+        horizontalSpacer = new QSpacerItem(531, 410, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        gridLayout_2->addItem(horizontalSpacer, 1, 1, 1, 1);
+
+        force_label = new QLabel(scene);
+        force_label->setObjectName(QString::fromUtf8("force_label"));
+
+        gridLayout_2->addWidget(force_label, 0, 1, 1, 1);
+
+
+        gridLayout->addWidget(scene, 0, 0, 1, 1);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 1215, 21));
+        menu_File = new QMenu(menubar);
+        menu_File->setObjectName(QString::fromUtf8("menu_File"));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menu_File->menuAction());
+        menu_File->addAction(action_close);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "Space Odyssey", 0, QApplication::UnicodeUTF8));
+        action_close->setText(QApplication::translate("MainWindow", "&Close", 0, QApplication::UnicodeUTF8));
+        action_close->setShortcut(QApplication::translate("MainWindow", "Ctrl+Q", 0, QApplication::UnicodeUTF8));
+        force_label->setText(QApplication::translate("MainWindow", "Force: ", 0, QApplication::UnicodeUTF8));
+        menu_File->setTitle(QApplication::translate("MainWindow", "&File", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // MOC_UIMAINWINDOW_H
