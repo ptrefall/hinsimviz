b2f4936a6fce00afb28dd24877affc75de61d815
PTrefall@gmail.com <PTrefall@gmail.com@28a56cb3-1e7c-bc60-7718-65c159e1d2df>
2010-09-01 07:44:33 +0000

    Updated engine. Now works with Lua, XML loading and the whole lot :) Input handling is not supported.

--- Src/Engine/Core/CoreManager.cpp
+++ Src/Engine/Core/CoreManager.cpp
@@ -1,15 +1,189 @@
 #include "CoreManager.h"
-#include <GUI/IGUIManager.h>
 
+#include <ClanLib/core.h>
+#include <GL/glew.h>
+#include <Engine/GUI/IGUIManager.h>
+#include <Engine/Resource/ResManager.h>
+#include <Engine/Log/LogManager.h>
+#include <Engine/Events/EngineEventManager.h>
+#include <Engine/Scene/ObjectManager.h>
+#include <Engine/Scene/SceneManager.h>
+#include <Engine/Script/ScriptManager.h>
+#include <Engine/Player/PlayerManager.h>
+#include <Engine/GameState/GameStateManager.h>
+#include <Engine/Events/EngineEvent.h>
+
+using namespace Engine;
 using namespace Core;
 
-CoreManager::CoreManager(GUI::IGuiManager *guiManager)
+CoreManager::CoreManager(int argc, char *argv[], GUI::IGuiManager *guiManager)
+: setupCore(new CL_SetupCore()), 
+  resMgr(NULL), sceneMgr(NULL), objMgr(NULL), playerMgr(NULL), gameStateMgr(NULL), scriptMgr(NULL),
+  engineEventMgr(NULL), logMgr(NULL)
 {
+	CL_String arg = argv[0];
+	arg = arg.substr(0, arg.find_last_of("\\"));
+	arg = arg.substr(0, arg.find_last_of("\\"));
+	arg = arg.substr(0, arg.find_last_of("\\"));
+	//arg = arg.substr(0, arg.find_last_of("\\"));
+	arg += "\\Resources\\";
+
 	this->guiManager = guiManager;
+	
+	try
+	{
+		init(arg.c_str());
+	}
+	catch(const CL_Exception &e)
+	{
+		if(logMgr)
+		{
+			logMgr->log("CoreManager::CoreManager", e.what(), Log::L_FATAL);
+		}
+		else
+		{
+			std::cout << "CL_EXCEPTION: " << e.what() << std::endl;
+		}
+	}
 }
 
 CoreManager::~CoreManager()
 {
+	if(resMgr)
+	{
+		delete resMgr;
+		resMgr = NULL;
+	}
+
+	/*if(inputMgr)
+	{
+		delete inputMgr;
+		inputMgr = NULL;
+	}*/
+
+	if(sceneMgr)
+	{
+		delete sceneMgr;
+		sceneMgr = NULL;
+	}
+
+	if(objMgr)
+	{
+		delete objMgr;
+		objMgr = NULL;
+	}
+
+	/*if(soundMgr)
+	{
+		delete soundMgr;
+		soundMgr = NULL;
+	}*/
+
+	if(playerMgr)
+	{
+		delete playerMgr;
+		playerMgr = NULL;
+	}
+
+	if(gameStateMgr)
+	{
+		delete gameStateMgr;
+		gameStateMgr = NULL;
+	}
+
+	if(scriptMgr)
+	{
+		delete scriptMgr;
+		scriptMgr = NULL;
+	}
+
+	if(engineEventMgr)
+	{
+		delete engineEventMgr;
+		engineEventMgr = NULL;
+	}
+
+	if(logMgr)
+	{
+		delete logMgr;
+		logMgr = NULL;
+	}
+
+	delete setupCore;
+	setupCore = NULL;
+}
+
+void CoreManager::init(const char *arg)
+{
+	//RESOURCE MANAGER //////////////////////////////////////////////////////////////////////
+	resMgr = new Resource::ResManager();
+	int fail = resMgr->init(arg);
+	if(fail)
+		throw CL_Exception("Initializing the Resource Manager failed!");
+
+	//LOG MANAGER //////////////////////////////////////////////////////////////////////
+	logMgr = new Engine::Log::LogManager(this);
+	fail = logMgr->init();
+	if(fail)
+		throw CL_Exception("Initializing the Log Manager failed!");
+
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if(err != GLEW_OK)
+	{
+		const unsigned char *errString = glewGetErrorString(err);
+		CL_String error = cl_format("Error loading GLEW: %1", (const char*)errString);
+		logMgr->log("AppManager:Init", error, Log::L_FATAL);
+		throw CL_Exception("Initializing GLEW failed!");
+	}
+
+	//INPUT MANAGER //////////////////////////////////////////////////////////////////////
+	/*inputMgr = new Engine::Input::OISManager(this);
+	fail = inputMgr->init();
+	if(fail)
+		throw CL_Exception("Initializing the Input Manager failed!");*/
+
+	//ENGINE EVENT MANAGER //////////////////////////////////////////////////////////////////////
+	engineEventMgr = new Events::EngineEventManager();
+	fail = engineEventMgr->init();
+	if(fail)
+		throw CL_Exception("Initializing the Engine Event Manager failed!");
+
+	//SOUND MANAGER //////////////////////////////////////////////////////////////////////
+	/*soundMgr = new Engine::Sound::SoundManager(this);
+	fail = soundMgr->init();
+	if(fail)
+		throw CL_Exception("Initializing the Sound Manager failed!");*/
+
+	//SCENE MANAGER //////////////////////////////////////////////////////////////////////
+	sceneMgr = new Engine::Scene::SceneManager(this);
+	fail = sceneMgr->init();
+	if(fail)
+		throw CL_Exception("Initializing the Scene Manager failed!");
+
+	//OBJECT MANAGER //////////////////////////////////////////////////////////////////////
+	objMgr = new Engine::Scene::ObjectManager(this);
+	fail = objMgr->init();
+	if(fail)
+		throw CL_Exception("Initializing the Object Manager failed!");
+
+	//PLAYER MANAGER //////////////////////////////////////////////////////////////////////
+	playerMgr = new Engine::Player::PlayerManager(this);
+	fail = playerMgr->init();
+	if(fail)
+		throw CL_Exception("Initializing the Player Manager failed!");
+
+	//SCRIPT MANAGER //////////////////////////////////////////////////////////////////////
+	scriptMgr = new Engine::Script::ScriptManager(this);
+	fail = scriptMgr->init();
+	if(fail)
+		throw CL_Exception("Initializing the Script Manager failed!");
+
+	//GAME STATE MANAGER //////////////////////////////////////////////////////////////////////
+	gameStateMgr = new Engine::GameState::GameStateManager(this);
+	fail = gameStateMgr->init();
+	if(fail)
+		throw CL_Exception("Initializing the Game State Manager failed!");
 }
 
 void CoreManager::frame()
--- Src/Engine/Core/CoreManager.h
+++ Src/Engine/Core/CoreManager.h
@@ -1,18 +1,66 @@
 #pragma once
 
-namespace GUI { class IGuiManager; }
+class CL_SetupCore;
 
+namespace Engine
+{
+namespace GUI { class IGuiManager; }
+namespace Resource { class ResManager; }
+namespace Log { class LogManager; }
+namespace Events { class IEngineEventManager; }
+namespace Scene { class SceneManager; class ObjectManager; }
+namespace Script { class ScriptManager; }
+//namespace Sound { class SoundManager; }
+namespace Player { class PlayerManager; }
+namespace GameState { class GameStateManager; }
 namespace Core
 {
 	class CoreManager
 	{
 	public:
-		CoreManager(GUI::IGuiManager *guiManager);
+		CoreManager(int argc, char *argv[], GUI::IGuiManager *guiManager);
 		~CoreManager();
 
 		void frame();
+		void exit() {}
+
+		GUI::IGuiManager *getGuiMgr() const { return guiManager; }
+		Resource::ResManager *getResMgr() const { return resMgr; }
+		Events::IEngineEventManager *getEngineEventMgr() const { return engineEventMgr; }
+		Log::LogManager *getLogMgr() const { return logMgr; }
+		//Input::IManager *getInputMgr() const { return inputMgr; }
+		Scene::ObjectManager *getObjectMgr() const { return objMgr; }
+		Scene::SceneManager *getSceneMgr() const { return sceneMgr; }
+		Script::ScriptManager *getScriptMgr() const { return scriptMgr; }
+		//Sound::SoundManager *getSoundMgr() const { return soundMgr; }
+		Player::PlayerManager *getPlayerMgr() const { return playerMgr; }
+		GameState::GameStateManager *getGameStateMgr() const { return gameStateMgr; }
+
+		//Utility::ITimer *getTimer() const { return timer; }
 
 	private:
+		void init(const char *arg);
+
 		GUI::IGuiManager *guiManager;
+
+		Resource::ResManager *resMgr;
+		Events::IEngineEventManager *engineEventMgr;
+		Log::LogManager *logMgr;
+		//Input::IManager *inputMgr;
+		Scene::ObjectManager *objMgr;
+		Scene::SceneManager *sceneMgr;
+		Script::ScriptManager *scriptMgr;
+		//Sound::SoundManager *soundMgr;
+		Player::PlayerManager *playerMgr;
+		GameState::GameStateManager *gameStateMgr;
+
+		CL_SetupCore *setupCore; // Initializes clanlib core lib when CoreManager is instanciated/constructed
+
+		//Utility::ITimer *timer;
+		double timeAccumulator;
+		double timeStep;
+
+		bool isRunning;
 	};
+}
 }
