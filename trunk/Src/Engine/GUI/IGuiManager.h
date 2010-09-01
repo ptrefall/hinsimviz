4ee43d9f31b409a8364d267ee766c5e5e3f39746
PTrefall@gmail.com <PTrefall@gmail.com@28a56cb3-1e7c-bc60-7718-65c159e1d2df>
2010-09-01 07:44:33 +0000

    Updated engine. Now works with Lua, XML loading and the whole lot :) Input handling is not supported.

--- trunk/Src/Engine/GUI/IGuiManager.h
+++ trunk/Src/Engine/GUI/IGuiManager.h
@@ -1,10 +1,17 @@
 #pragma once
 
+namespace Engine
+{
 namespace GUI
 {
 	class IGuiManager
 	{
 	public:
 		virtual void center(int &x, int &y) = 0;
+		virtual int getWidth() const = 0;
+		virtual int getHeight() const = 0;
+		virtual int getDesktopWidth() const = 0;
+		virtual int getDesktopHeight() const = 0;
 	};
+}
 }
